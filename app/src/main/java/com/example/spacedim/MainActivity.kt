ffc5301d373ae04dec96f098e050da3c952f79a0
Baptiste TAVIOT <baptiste.taviot@inventhys.com>
2021-12-23 19:58:51 +0100

    Waiting + Game Activity + Button link

--- app/src/main/java/com/example/spacedim/MainActivity.kt
+++ app/src/main/java/com/example/spacedim/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.spacedim
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+
+        val button = findViewById<Button>(R.id.launchButtonHomePage)
+        button.setOnClickListener{
+            val intent = Intent(this, WaitingRoomActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
